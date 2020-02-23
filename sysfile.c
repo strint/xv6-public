ea1d4769ea835bebd847a522462ad567320eb180
strint <xiaoyulink@gmail.com>
2020-02-24 03:06:51 +0800

    reading file system call

--- sysfile.c
+++ sysfile.c
@@ -16,6 +16,26 @@
 #include "file.h"
 #include "fcntl.h"
 
+//  argfd()从系统调用的参数从取出fd
+//  fdalloc()进程内找一个空的fd，把进程中的ofile的fd位置指向该文件
+//  sys_dup()从系统调用的第0个参数中获取fd，对齐调用filedup()
+//  sys_read()从系统调用参数中获取fd、读取的位置、读取的数量，做fileread()
+//  sys_write()
+//  sys_close()
+//  sys_fstat()
+//  sys_link() 在new的目录下创建一个链接指向old
+//      1 增加old的ref_count
+//      2 在new的父dir下面增加一个dir entry，指向old
+//  isdirempty()判断dir是否为空，看的是所有除了.和..之外是否还有费控的dir entry
+//  sys_unlink()从父目录移除该dir entry并减少对应inode的ref_count
+//  create()通用的创建文件方法，给open和mkdir用
+//  sys_open()创建或者打开一个inode，创建一个file指向该inode
+//  sys_mkdir()创建一个dir inode
+//  sys_mknod()创建设备
+//  sys_chdir()根据名字找到对应的inode，iput之前cwd的inode，把当前进程的cwd指向新的inode
+//  sys_exec()从系统调用参数中解析参数传递给exec
+//  sys_pipe()创建pipe
+
 // Fetch the nth word-sized system call argument as a file descriptor
 // and return both the descriptor and the corresponding struct file.
 static int
@@ -28,8 +48,10 @@ argfd(int n, int *pfd, struct file **pf)
     return -1;
   if(fd < 0 || fd >= NOFILE || (f=myproc()->ofile[fd]) == 0)
     return -1;
+  // pipe fd
   if(pfd)
     *pfd = fd;
+  // fd
   if(pf)
     *pf = f;
   return 0;
@@ -44,6 +66,8 @@ fdalloc(struct file *f)
   struct proc *curproc = myproc();
 
   for(fd = 0; fd < NOFILE; fd++){
+    // 找到ofile中的一个空的位置返回
+    // fd是进程内编号的
     if(curproc->ofile[fd] == 0){
       curproc->ofile[fd] = f;
       return fd;
@@ -171,6 +195,7 @@ isdirempty(struct inode *dp)
   int off;
   struct dirent de;
 
+  // 偏移的前2个dir entry分别是.和..
   for(off=2*sizeof(de); off<dp->size; off+=sizeof(de)){
     if(readi(dp, (char*)&de, off, sizeof(de)) != sizeof(de))
       panic("isdirempty: readi");
@@ -245,19 +270,23 @@ create(char *path, short type, short major, short minor)
   struct inode *ip, *dp;
   char name[DIRSIZ];
 
+  // 找到path倒数第二dir的inode
   if((dp = nameiparent(path, name)) == 0)
     return 0;
   ilock(dp);
 
+  // 从父dir中查找出该dir的node
   if((ip = dirlookup(dp, name, &off)) != 0){
     iunlockput(dp);
     ilock(ip);
+    // 能找到就返回
     if(type == T_FILE && ip->type == T_FILE)
       return ip;
     iunlockput(ip);
     return 0;
   }
 
+  // 没找到就分配一个inode
   if((ip = ialloc(dp->dev, type)) == 0)
     panic("create: ialloc");
 
@@ -271,10 +300,12 @@ create(char *path, short type, short major, short minor)
     dp->nlink++;  // for ".."
     iupdate(dp);
     // No ip->nlink++ for ".": avoid cyclic ref count.
+    // 目录类型创建.和..两个link
     if(dirlink(ip, ".", ip->inum) < 0 || dirlink(ip, "..", dp->inum) < 0)
       panic("create dots");
   }
 
+  // 把新创建的加到父dir下面
   if(dirlink(dp, name, ip->inum) < 0)
     panic("create: dirlink");
 
@@ -429,9 +460,12 @@ sys_pipe(void)
 
   if(argptr(0, (void*)&fd, 2*sizeof(fd[0])) < 0)
     return -1;
+  // 创建了两个file
+  // pipe file，没有inode
   if(pipealloc(&rf, &wf) < 0)
     return -1;
   fd0 = -1;
+  // 分别为其分配fd
   if((fd0 = fdalloc(rf)) < 0 || (fd1 = fdalloc(wf)) < 0){
     if(fd0 >= 0)
       myproc()->ofile[fd0] = 0;
